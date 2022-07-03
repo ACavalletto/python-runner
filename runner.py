3178a351524fb9b4cb44297b0c467d48744e8cb5
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-03 17:02:15 -0600

    added window display as well as ability to exit window

--- runner.py
+++ runner.py
@@ -1,2 +1,15 @@
 import pygame
 
+pygame.init()
+
+#Creates a display window for the game.
+screen = pygame.display.set_mode((800, 400)) 
+
+while True:
+    # gets all events and loops through them
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #checks for exit button for window
+            pygame.quit()
+    # draw all our elements
+    # Updates the display window
+    pygame.display.update()

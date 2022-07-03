eb3746858bb79cf545132f9545420b673b49a598
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-03 17:26:47 -0600

    added graphics assets

--- runner.py
+++ runner.py
@@ -1,15 +1,25 @@
 import pygame
+from sys import exit
 
 pygame.init()
 
 #Creates a display window for the game.
 screen = pygame.display.set_mode((800, 400)) 
+pygame.display.set_caption('Runner') 
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
 
 while True:
     # gets all events and loops through them
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #checks for exit button for window
             pygame.quit()
-    # draw all our elements
+            exit() # exits while loop
+    
+    screen.blit(test_surface, (200,100))
+    
     # Updates the display window
     pygame.display.update()
+    clock.tick(60) #caps framerate to 60 fps

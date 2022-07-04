4ab9e00095fc8689459bbc193463126d438843e6
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-04 16:52:49 -0600

    randomly generate a snail or fly to come across screen

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 def display_score():
     current_time = int(pygame.time.get_ticks() / 1000) - start_time
@@ -8,6 +9,17 @@ def display_score():
     screen.blit(score_surf, score_rect)
     return current_time
 
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+            if obstacle_rect.bottom == 300: screen.blit(snail_surf, obstacle_rect)
+            else: screen.blit(fly_surf, obstacle_rect)
+            # deletes obstacle rects when they go to far off screen
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        return obstacle_list
+    else: return []
+    
 pygame.init()
 
 #Creates a display window for the game.
@@ -28,8 +40,12 @@ ground_surf = pygame.image.load('graphics/ground.png').convert()
 # score_rect = score_surf.get_rect(center= (400, 50))
 
 snail_surf = pygame.image.load('graphics//snail/snail1.png').convert_alpha()
-snail_rect = snail_surf.get_rect(bottomright = (600,300))
 
+fly_surf = pygame.image.load('graphics/fly/Fly1.png').convert_alpha()
+
+
+
+obstacle_rect_list = []
 
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 # adds a rectangle around player surface that allows you to position image easier
@@ -48,6 +64,9 @@ game_name_rect = game_name.get_rect(center = (400,80))
 instructions_surf = test_font.render(f'Press Space To Start.', False, (111,196,169))
 instructions_rect = instructions_surf.get_rect(center = (400, 330))
 
+# timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
 
 while True:
     # gets all events and loops through them
@@ -67,22 +86,24 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                snail_rect.left = 800
                 start_time =  int(pygame.time.get_ticks() / 1000)
+                
+        if event.type == obstacle_timer and game_active:
+            if randint(0,2):
+                obstacle_rect_list.append(snail_surf.get_rect(bottomright = (randint(900,1100),300)))
+            else: 
+                obstacle_rect_list.append(fly_surf.get_rect(bottomright = (randint(900,1100),210)))
                 
     if game_active:       #active game code
         #display sky surface on display surface
         screen.blit(sky_surf, (0,0)) 
         screen.blit(ground_surf, (0,300))
         
-        # pygame.draw.rect(screen, '#c0e8ec',score_rect)
-        # pygame.draw.rect(screen, '#c0e8ec',score_rect,10)
-        # screen.blit(score_surf, score_rect)
         score = display_score()
         
-        snail_rect.x -= 4
-        if snail_rect.right <= 0:snail_rect.left = 800   
-        screen.blit(snail_surf, snail_rect)
+        # snail_rect.x -= 4
+        # if snail_rect.right <= 0:snail_rect.left = 800   
+        # screen.blit(snail_surf, snail_rect)
         
         player_gravity += 1
         player_rect.y += player_gravity
@@ -90,17 +111,17 @@ while True:
         if player_rect.bottom >= 300: player_rect.bottom = 300
         screen.blit(player_surf, player_rect)
         
+        # Obstacle Movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+        
         # collision
-        if snail_rect.colliderect(player_rect):
-            game_active = False
     else:
         screen.fill((94, 129, 162))
         screen.blit(player_stand, player_stand_rect)
         screen.blit(game_name, game_name_rect)
         score_surf = test_font.render(f'Score: {score}', False, (111,196,169))
         score_rect = score_surf.get_rect(center = (400, 330))
-        if score == 0:
-            screen.blit(instructions_surf, instructions_rect)
+        if score == 0: screen.blit(instructions_surf, instructions_rect)
         else: screen.blit(score_surf, score_rect)
     # Updates the display window
     pygame.display.update()

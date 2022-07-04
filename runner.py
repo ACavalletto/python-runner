9ecde876f18d7240cca539741d20dfabc32c2a30
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-03 22:28:37 -0600

    refactored snail to use a rect

--- runner.py
+++ runner.py
@@ -15,7 +15,12 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
 
 snail_surface = pygame.image.load('graphics//snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(bottomright = (600,300))
+
+
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+# adds a rectangle around player surface that allows you to position image easier
+player_rect = player_surf.get_rect(midbottom=(80,300))
 
 while True:
     # gets all events and loops through them
@@ -28,10 +33,11 @@ while True:
     screen.blit(sky_surface, (0,0)) 
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface, (300, 50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos,250))
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800   
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surf, player_rect)
     
     # Updates the display window
     pygame.display.update()

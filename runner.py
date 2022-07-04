003b6db4e4695962d0a5cbc5643305fcdf7524df
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-04 15:53:22 -0600

    added end game state if snail collides with player and a game restart if on game over screen

--- runner.py
+++ runner.py
@@ -8,6 +8,7 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner') 
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = True
 
 # import sky.png as a surface object
 sky_surf = pygame.image.load('graphics/sky.png').convert() 
@@ -33,35 +34,40 @@ while True:
             exit() # exits while loop
             
         # if statements that check for button press or mouse click to jump character and character is on the floor
-        if event.type == pygame.MOUSEBUTTONDOWN:
-           if  player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
-               player_gravity = -20
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                player_gravity = -20
-                
-    #display sky surface on display surface
-    screen.blit(sky_surf, (0,0)) 
-    screen.blit(ground_surf, (0,300))
-    
-    pygame.draw.rect(screen, '#c0e8ec',score_rect)
-    pygame.draw.rect(screen, '#c0e8ec',score_rect,10)
-    screen.blit(score_surf, score_rect)
-    
-    snail_rect.x -= 4
-    if snail_rect.right <= 0:snail_rect.left = 800   
-    screen.blit(snail_surf, snail_rect)
-    
-    player_gravity += 1
-    player_rect.y += player_gravity
-    # keeps player on the "floor" and if not jumping
-    if player_rect.bottom >= 300: player_rect.bottom = 300
-    screen.blit(player_surf, player_rect)
-    
-    # collision
-    if snail_rect.colliderect(player_rect):
-        pygame.quit()
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if  player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+    if game_active:       #active game code
+        #display sky surface on display surface
+        screen.blit(sky_surf, (0,0)) 
+        screen.blit(ground_surf, (0,300))
         
+        pygame.draw.rect(screen, '#c0e8ec',score_rect)
+        pygame.draw.rect(screen, '#c0e8ec',score_rect,10)
+        screen.blit(score_surf, score_rect)
+        
+        snail_rect.x -= 4
+        if snail_rect.right <= 0:snail_rect.left = 800   
+        screen.blit(snail_surf, snail_rect)
+        
+        player_gravity += 1
+        player_rect.y += player_gravity
+        # keeps player on the "floor" and if not jumping
+        if player_rect.bottom >= 300: player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
+        
+        # collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('Yellow')
     # Updates the display window
     pygame.display.update()
     clock.tick(60) #caps framerate to 60 fps

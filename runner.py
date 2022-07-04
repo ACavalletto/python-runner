7457ff1d1d69db94d2b1c9a7cb1ee90f81fb274d
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-03 22:12:03 -0600

    added a moving snail component to game

--- runner.py
+++ runner.py
@@ -7,13 +7,15 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400)) 
 pygame.display.set_caption('Runner') 
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 # import sky.png as a surface object
-sky_surface = pygame.image.load('graphics/sky.png') 
-ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('My game', False, 'Green')
+sky_surface = pygame.image.load('graphics/sky.png').convert() 
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, 'Black')
 
+snail_surface = pygame.image.load('graphics//snail/snail1.png').convert_alpha()
+snail_x_pos = 600
 
 while True:
     # gets all events and loops through them
@@ -26,6 +28,10 @@ while True:
     screen.blit(sky_surface, (0,0)) 
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface, (300, 50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos,250))
     
     # Updates the display window
     pygame.display.update()

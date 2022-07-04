aa084245b64ea4def907c13fa627974951339f17
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-04 15:45:25 -0600

    added jumping mechanic to player on key press or mouse click

--- runner.py
+++ runner.py
@@ -13,7 +13,7 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 sky_surf = pygame.image.load('graphics/sky.png').convert() 
 ground_surf = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render('My game', False, 'Black')
+score_surf = test_font.render('My game', False, (64,64,64))
 score_rect = score_surf.get_rect(center= (400, 50))
 
 snail_surf = pygame.image.load('graphics//snail/snail1.png').convert_alpha()
@@ -23,6 +23,7 @@ snail_rect = snail_surf.get_rect(bottomright = (600,300))
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 # adds a rectangle around player surface that allows you to position image easier
 player_rect = player_surf.get_rect(midbottom=(80,300))
+player_gravity = 0
 
 while True:
     # gets all events and loops through them
@@ -30,25 +31,37 @@ while True:
         if event.type == pygame.QUIT: #checks for exit button for window
             pygame.quit()
             exit() # exits while loop
-        # if event.type == pygame.MOUSEMOTION:
-        #    if  player_rect.collidepoint(event.pos): print('collision')
+            
+        # if statements that check for button press or mouse click to jump character and character is on the floor
+        if event.type == pygame.MOUSEBUTTONDOWN:
+           if  player_rect.collidepoint(event.pos) and player_rect.bottom >= 300: 
+               player_gravity = -20
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -20
+                
     #display sky surface on display surface
     screen.blit(sky_surf, (0,0)) 
     screen.blit(ground_surf, (0,300))
+    
+    pygame.draw.rect(screen, '#c0e8ec',score_rect)
+    pygame.draw.rect(screen, '#c0e8ec',score_rect,10)
     screen.blit(score_surf, score_rect)
+    
     snail_rect.x -= 4
-    if snail_rect.right <= 0:
-        snail_rect.left = 800   
+    if snail_rect.right <= 0:snail_rect.left = 800   
     screen.blit(snail_surf, snail_rect)
+    
+    player_gravity += 1
+    player_rect.y += player_gravity
+    # keeps player on the "floor" and if not jumping
+    if player_rect.bottom >= 300: player_rect.bottom = 300
     screen.blit(player_surf, player_rect)
     
-    #checks if there is a collision between two rects
-    # if player_rect.colliderect(snail_rect):
-    #     print('collision')
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint((mouse_pos)):
-    #    print(pygame.mouse.get_pressed())
-    
+    # collision
+    if snail_rect.colliderect(player_rect):
+        pygame.quit()
+        
     # Updates the display window
     pygame.display.update()
     clock.tick(60) #caps framerate to 60 fps

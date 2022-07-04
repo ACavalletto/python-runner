ed711024347d96faefa04778aa8567d90661eee0
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-04 16:06:06 -0600

    updated score to display in single digits and accrue slower

--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@ import pygame
 from sys import exit
 
 def display_score():
-    current_time = pygame.time.get_ticks() - start_time
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
     score_surf = test_font.render(f'{current_time}', False, (64,64,64))
     score_rect = score_surf.get_rect(center =(400,50))
     screen.blit(score_surf, score_rect)
@@ -52,7 +52,7 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
-                start_time = pygame.time.get_ticks()
+                start_time =  int(pygame.time.get_ticks() / 1000)
                 
     if game_active:       #active game code
         #display sky surface on display surface
